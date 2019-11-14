1ad288351264bae19640648f1b9c8e4cc8ad783c
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-14 17:11:31 +0100

    wip mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ingredients = Ingredient.create([{name: "lemon"},
+                                  {name: "ice"},
+                                  {name: "mint leaves"},
+                                  {name: "huzo"},
+                                  {name: "cherry"},
+                                  {name: "soda"}])
